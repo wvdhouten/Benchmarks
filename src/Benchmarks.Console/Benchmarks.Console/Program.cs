a3f58eb70f261608ea7cf832cfa227e2b5817503
Walter van den Houten <waltervdhouten@gmail.com>
2024-03-11 22:51:21 +0200

    removed comment

--- src/Benchmarks.Console/Benchmarks.Console/Program.cs
+++ src/Benchmarks.Console/Benchmarks.Console/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Running;
 using Benchmarks.Console.Benchmarks;
 
 BenchmarkRunner.Run<Hashing>();
